814cf4ac7a15d3dd97a6bf057720fa815ded88e6
Igor Sfiligoi <isfiligoi@ucsd.edu>
2022-02-28 11:21:15 -0800

    Add support for DEP_REPOS

--- image-nodeplugin/mount-and-wait.sh
+++ image-nodeplugin/mount-and-wait.sh
@@ -15,8 +15,46 @@ fi
 
 
 if [ "x${MOUNT_REPOS}" == "x" ]; then
-  echo "Missing MOUNT_REPOS" 1>&2
+  echo "ERROR: `date` Missing MOUNT_REPOS" 1>&2
   exit 1
+fi
+
+if [ "x${DEP_REPOS}" != "x" ]; then
+  for d in `echo ${DEP_REPOS} |tr , ' '` ; do
+    if [ ! -d "/cvmfs/${d}" ]; then
+      echo "INFO: `date` Not yet ready /cvmfs/${d}. Waiting." | tee -a /cvmfs/cvmfs-pod.log
+      sleep 1
+    fi
+    if [ ! -d "/cvmfs/${d}" ]; then
+      echo "INFO: `date` Not yet ready /cvmfs/${d}. Waiting." | tee -a /cvmfs/cvmfs-pod.log
+      sleep 2
+    fi
+    if [ ! -d "/cvmfs/${d}" ]; then
+      echo "INFO: `date` Not yet ready /cvmfs/${d}. Waiting." | tee -a /cvmfs/cvmfs-pod.log
+      sleep 5
+    fi
+    if [ ! -d "/cvmfs/${d}" ]; then
+      echo "ERROR: `date` Not yet ready /cvmfs/${d}. Aborting." | tee -a /cvmfs/cvmfs-pod.log
+      exit 1
+    fi
+    if [ "`ls /cvmfs/${d} |wc -l`" -lt 2 ]; then
+      echo "INFO: `date` Still empty /cvmfs/${d}. Waiting." | tee -a /cvmfs/cvmfs-pod.log
+      sleep 1
+    fi
+    if [ "`ls /cvmfs/${d} |wc -l`" -lt 2 ]; then
+      echo "INFO: `date` Still empty /cvmfs/${d}. Waiting." | tee -a /cvmfs/cvmfs-pod.log
+      sleep 2
+    fi
+    if [ "`ls /cvmfs/${d} |wc -l`" -lt 2 ]; then
+      echo "INFO: `date` Still empty /cvmfs/${d}. Waiting." | tee -a /cvmfs/cvmfs-pod.log
+      sleep 5
+    fi
+    if [ "`ls /cvmfs/${d} |wc -l`" -lt 2 ]; then
+      echo "ERROR: `date` Still empty /cvmfs/${d}. Aborting." | tee -a /cvmfs/cvmfs-pod.log
+      exit 1
+    fi
+    echo "INFO: `date` Dependency ready: /cvmfs/${d}." | tee -a /cvmfs/cvmfs-pod.log
+  done
 fi
 
 # do not die on signal, try to cleanup as fast as you can
@@ -24,12 +62,12 @@ trap "/usr/local/sbin/force_unmount.sh" SIGTERM SIGINT
 
 mps=""
 for mp in `echo ${MOUNT_REPOS} |tr , ' '` ; do 
- echo "INFO: `date` Processing /cvmfs/${mp}." | tee -a /cvmfs/cvmfs-pod.log
+ echo "INFO: `date` `date` Processing /cvmfs/${mp}." | tee -a /cvmfs/cvmfs-pod.log
 
  mkdir -p /cvmfs/${mp}
  rc=$?
  if [ ${rc} -ne 0 ]; then
-   echo "INFO: Removing existing /cvmfs/${mp}." | tee -a /cvmfs/cvmfs-pod.log
+   echo "INFO: `date` Removing existing /cvmfs/${mp}." | tee -a /cvmfs/cvmfs-pod.log
    rmdir /cvmfs/${mp}
    # no error checking ... if it failed, we will catch below
    mkdir -p /cvmfs/${mp}
@@ -38,11 +76,11 @@ for mp in `echo ${MOUNT_REPOS} |tr , ' '` ; do
 
  if [ ${rc} -ne 0 ]; then
    # force clean if already there
-   echo "WARNING: Found /cvmfs/${mp}. Unmounting." | tee -a /cvmfs/cvmfs-pod.log
+   echo "WARNING: `date` Found /cvmfs/${mp}. Unmounting." | tee -a /cvmfs/cvmfs-pod.log
    umount /cvmfs/${mp}
    rc=$?
    if [ $rc -ne 0 ]; then
-     echo "WARNING: Using lazy umount for /cvmfs/${mp}" | tee -a /cvmfs/cvmfs-pod.log
+     echo "WARNING: `date` Using lazy umount for /cvmfs/${mp}" | tee -a /cvmfs/cvmfs-pod.log
      umount -l /cvmfs/${mp}
      sleep 5 # give time for the system to catch up
    fi
@@ -57,7 +95,7 @@ for mp in `echo ${MOUNT_REPOS} |tr , ' '` ; do
  mount -t cvmfs ${mp} /cvmfs/${mp}
  rc=$?
  if [ ${rc} -ne 0 ] ; then
-   echo "WARNING: Failed to mount $mp, retrying"  | tee -a /cvmfs/cvmfs-pod.log
+   echo "WARNING: `date` Failed to mount $mp, retrying"  | tee -a /cvmfs/cvmfs-pod.log
    sleep 15
    mkdir -p /cvmfs/${mp}
    mount -t cvmfs ${mp} /cvmfs/${mp}
@@ -65,13 +103,13 @@ for mp in `echo ${MOUNT_REPOS} |tr , ' '` ; do
  fi
 
  if [ ${rc} -eq 0 ] ; then
-   echo "INFO: Mounted /cvmfs/${mp}" | tee -a /cvmfs/cvmfs-pod.log
+   echo "INFO: `date` Mounted /cvmfs/${mp}" | tee -a /cvmfs/cvmfs-pod.log
    # apart from info, this also warms up the mount
    echo "INFO: N. Files: `ls /cvmfs/${mp} |wc -l`" | tee -a /cvmfs/cvmfs-pod.log
    echo "INFO: N. etc Files: `find /cvmfs/${mp}/etc 2>/dev/null |wc -l`" | tee -a /cvmfs/cvmfs-pod.log
    mps="$mp $mps" #save them in reverse order
  else
-   echo "ERROR: Failed to mount $mp"  | tee -a /cvmfs/cvmfs-pod.log
+   echo "ERROR: `date` Failed to mount $mp"  | tee -a /cvmfs/cvmfs-pod.log
 
    # cleanup
    for mp1 in $mps; do
@@ -95,10 +133,10 @@ for mp1 in $mps; do
      umount /cvmfs/${mp1}
      rc=$?
      if [ $rc -ne 0 ]; then
-       echo "WARNING: Failed unmounting ${mp1}"  | tee -a /cvmfs/cvmfs-pod.log
+       echo "WARNING: `date` Failed unmounting ${mp1}"  | tee -a /cvmfs/cvmfs-pod.log
      else
        rmdir /cvmfs/${mp1}
-       echo "INFO: Unmounted ${mp1}"  | tee -a /cvmfs/cvmfs-pod.log
+       echo "INFO: `date` Unmounted ${mp1}"  | tee -a /cvmfs/cvmfs-pod.log
      fi
    fi
 done
@@ -106,10 +144,10 @@ done
 # now do a pass with the most fail-safe option possible
 for mp1 in $mps; do
    if [ -d /cvmfs/${mp1} ]; then
-     echo "INFO: Attempting lazy umount of ${mp1}"  | tee -a /cvmfs/cvmfs-pod.log
+     echo "INFO: `date` Attempting lazy umount of ${mp1}"  | tee -a /cvmfs/cvmfs-pod.log
      umount -l /cvmfs/${mp1}
      if [ $? -eq 0 ]; then
-       echo "INFO: Lazy unmounted ${mp1}"  | tee -a /cvmfs/cvmfs-pod.log
+       echo "INFO: `date` Lazy unmounted ${mp1}"  | tee -a /cvmfs/cvmfs-pod.log
      fi
      # try to remove mount dir no matter what, to minimize chance of PVC mounting an inactive dir
      rmdir /cvmfs/${mp1}
